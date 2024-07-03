b860d0c3537f77e47d0711e398638a7edf53af9a
mahesh989 <maheshtwari99@gmail.com>
2024-07-03 11:28:04 +1000

    query modified

--- Weather/sql/basic_queries.sql
+++ Weather/sql/basic_queries.sql
@@ -1,62 +1,112 @@
---- the number of rows
-select count(*) from air_quality;
-
---- save the data in csv format by downloading
+--- Part 1 
+--- Check for Sample Data
 select * from air_quality;
 
---- the number of columns
-select count(*) 
+--- Row Count
+select count(*) from air_quality;
+
+--- Column Count
+select count(*)
 from information_schema.columns 
 where table_name = 'air_quality';
 
-
---- shape
+--- Shape (Row Count and Column Count Combined)
 select 
-(select count(*) from air_quality) as row_count,
-(select count(*) 
+	(select count(*) as row_count from air_quality),
+	(select count(*) as column_count
 from information_schema.columns 
-where table_name = 'air_quality') as column_count;
+where table_name = 'air_quality');
 
-
---- column names
+--- Column Names
 select column_name
-from information_schema.columns 
+from information_schema.columns
 where table_name = 'air_quality';
 
---- Convert Entries to Lowercase
-UPDATE air_quality
-SET location_in_Aus = LOWER(location_in_Aus);
+--- lower entries  and trimming , COMMON TABLE EXPRESSION
+update air_quality 
+set location_in_aus = upper(trim(location_in_aus));
+select *from air_quality;
 
----Remove Leading and Trailing Spaces
-UPDATE air_quality
-SET location_in_Aus = TRIM(location_in_Aus);
-
---- to find unique values in a particular column
-SELECT DISTINCT parameter as distinct_parameters
-FROM air_quality;
-
---- to find number of unique values in a particular column
-SELECT count(DISTINCT parameter) as number_of_distinct_parameters
-FROM air_quality;
+WITH updated AS (
+    UPDATE air_quality 
+    SET location_in_aus = upper(TRIM(location_in_aus))
+    RETURNING *
+)
+SELECT * FROM updated;
 
 
---- removing time-stamp
-ALTER TABLE air_quality
-ALTER COLUMN date_utc TYPE DATE
-USING date(date_utc);
 
----  number of locations by count
-SELECT location_in_aus, COUNT(*)
+--- Sort Records in Ascending Order,descending Order
+select * from air_quality 
+order by value desc
+limit 10;
+
+--- IS NULL Check 
+select * from air_quality
+where value is null and parameter is null;
+
+--- IS NOT NULL Check
+select * from air_quality
+where location_in_aus is not null and value is not null;
+
+---  duplicate check
+select  location_in_aus, count(*) from air_quality
+group by location_in_aus
+having count(*) > 1;
+
+SELECT longitude, location_in_aus, COUNT(*) AS count
 FROM air_quality
-GROUP BY location_in_aus
-order by count desc;
+GROUP BY longitude, location_in_aus
+HAVING COUNT(*) > 1;
 
 
----
-SELECT location_in_aus, AVG(value) AS average_value
-FROM air_quality
-GROUP BY location_in_aus
-ORDER BY average_value DESC;
+--- Find Distinct Entries
+select distinct unit from air_quality;
+
+--- Count for Distinct Entries
+select count(distinct parameter) as unique_parametr_number from air_quality;
+
+--- Minimum Value of a Column
+select min(value) from air_quality;
+
+--- Maximum Value of a Column
+select max(value) from air_quality;
+
+--- Average Value of a Column
+select avg(value) from air_quality;
+
+--- Sum of a Column
+select sum(value) from air_quality;
+
+
+
+
+--- for practice next time 
+--- Check for Sample Data
+--- Row Count
+--- Column Count
+--- Shape (Row Count and Column Count Combined)
+--- Column Names
+--- Lower Entries and Trimming (Common Table Expression)
+--- Sort Records in Ascending Order, Descending Order
+--- IS NULL Check
+--- IS NOT NULL Check
+--- Duplicate Check
+--- Find Distinct Entries
+--- Count for Distinct Entries
+--- Minimum Value of a Column
+--- Maximum Value of a Column
+--- Average Value of a Column
+--- Sum of a Column
+
+
+
+
+
+
+
+
+
 
 
 
